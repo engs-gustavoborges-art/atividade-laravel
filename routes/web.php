416d24d73748bc61c5015aaedce4ac1274e1cfe6
Gustavo <engs-gustavoborges@camporeal.edu.br>
2026-09-09 22:21:46 -0300

    ATV1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/sobre', function () {
+    return 'Página Sobre';
+});
+
+Route::get('/alunos', function () {
+    return 'Página de Alunos';
+});
+
+Route::get('/contato', function () {
+    return 'Página de Contato';
 });
