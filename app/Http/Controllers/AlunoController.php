d09dcb6ee67b68037cabc012a1ba2c082e0c0716
Gustavo <engs-gustavoborges@camporeal.edu.br>
2026-09-09 22:40:15 -0300

    ATV4

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'Lista de alunos';
+    }
+
+    public function show($id)
+    {
+        return 'Exibindo aluno: ' . $id;
+    }
+
+    public function create()
+    {
+        return 'Formulário para criar aluno';
+    }
+
+    public function store(Request $request)
+    {
+        return 'Aluno cadastrado com sucesso';
+    }
+
+    public function edit($id)
+    {
+        return 'Editando aluno: ' . $id;
+    }
+
+    public function update(Request $request, $id)
+    {
+        return 'Aluno atualizado: ' . $id;
+    }
+
+    public function destroy($id)
+    {
+        return 'Aluno excluído: ' . $id;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
 
 Route::get('/sobre', function () {
     return 'Página Sobre';
@@ -25,3 +26,17 @@ Route::get('/categoria/{id}', function ($id) {
 Route::get('/usuario/{id}', function ($id) {
     return 'Usuário: ' . $id;
 });
+
+Route::get('/alunos', [AlunoController::class, 'index']);
+
+Route::get('/alunos/{id}', [AlunoController::class, 'show']);
+
+Route::get('/alunos/create', [AlunoController::class, 'create']);
+
+Route::post('/alunos', [AlunoController::class, 'store']);
+
+Route::get('/alunos/{id}/edit', [AlunoController::class, 'edit']);
+
+Route::put('/alunos/{id}', [AlunoController::class, 'update']);
+
+Route::delete('/alunos/{id}', [AlunoController::class, 'destroy']);
